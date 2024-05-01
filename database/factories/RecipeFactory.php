5e3eac7beb95cdfb4e068a98202a0e501efbc29e
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-05-01 16:45:30 +0200

    create relations betweeen users and recipes

---
+++ database/factories/RecipeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Recipe>
+ */
+class RecipeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'description' => $this->faker->sentence,
+            'image' => $this->faker->sentence,
+            'prep_time' => $this->faker->numerify('####'),
+            'creator_id' => User::factory()
+        ];
+    }
+}
