b0fc4dfd3fd2d132f9c3f590faf5ca32cf271ef9
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-05-01 10:36:05 +0200

    initial commit

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipe;
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    public function index(Request $request)
+    {
+        return response()->json(Recipe::all());
+    }
+}
---
+++ database/migrations/2024_05_01_080100_create_recipes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->integer('prep_time');
+//            $table->('ingredients'); we need reference to another table
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
